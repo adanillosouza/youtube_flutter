feb66a3ec01b7dc74fb67002da633f8ff8d4e6d7
adanillosouza <dylotrons@gmail.com>
2024-01-31 21:40:05 -0300

    flutter commit

--- lib/home.dart
+++ lib/home.dart
@@ -8,38 +8,77 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _currentBottomNavigationIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> screens = [
+      Text("Inicío"),
+      Text("Em alta"),
+      Text("Inscrições"),
+      Text("Biblioteca"),
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.grey
-        ),
+        iconTheme: const IconThemeData(color: Colors.grey),
         backgroundColor: Colors.white,
         title: Image.asset(
-            "assets/youtube.png",
+          "assets/youtube.png",
           width: 98,
           height: 22,
         ),
         actions: <Widget>[
           IconButton(
-              onPressed: (){
+              onPressed: () {
                 print("BOTÃO DE VIDEO");
               },
-              icon: const Icon(Icons.videocam)
-          ),
+              icon: const Icon(Icons.videocam)),
           IconButton(
-              onPressed: (){
+              onPressed: () {
                 print("BOTÃO DE SEARCH");
               },
-              icon: const Icon(Icons.search)
-          ),
+              icon: const Icon(Icons.search)),
           IconButton(
-              onPressed: (){
+              onPressed: () {
                 print("BOTÃO DE CONTA");
               },
-              icon: const Icon(Icons.account_circle)
-          )
+              icon: const Icon(Icons.account_circle))
+        ],
+      ),
+      body: screens[_currentBottomNavigationIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.red,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentBottomNavigationIndex,
+        onTap: (index){
+          setState(() {
+            _currentBottomNavigationIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            backgroundColor: Colors.orange,
+            label: "Inicío",
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.red,
+            label: "Em alta",
+            icon: Icon(Icons.whatshot),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue,
+            label: "Inscrições",
+            icon: Icon(Icons.subscriptions),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.yellow,
+            label: "Biblioteca",
+            icon: Icon(Icons.folder),
+          ),
         ],
       ),
     );
